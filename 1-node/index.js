2999a1a30f452de68e688fb97f1792f53ab73c88
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-07-26 10:56:10 -0500

    -

--- 1-node/index.js
+++ 1-node/index.js
@@ -4,7 +4,9 @@ const mySecret = "afj239823u9843w189sj"
 
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJtZXNzYWdlIjoiWW91IEdvdCBUaGlzISA8MyJ9.ffRDp9RX5pLycpcQbIrg2l9BGM0xQC8Z2Pd90QeO40E"
 
-const message = null
+const message = jwt.decode(token, mySecret).message
+
+console.log((message))
 
 if(message.constructor == String){
     console.log( "Great Work")
